e42c3786caa34eb5406c95be802bbd7bdae2c55e
OmarTalaat221 <omartalaat1276@gmail.com>
2026-02-08 17:02:43 +0200

    routing , home,banners

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -3,8 +3,9 @@ import Project from "~/sections/Project/Project";
 import HeaderOne from "~/sections/Common/Header/HeaderOne";
 import FooterTwo from "~/sections/Common/Footer/FooterTwo";
 import Scroll from "~/sections/Common/Scroll";
-import HeaderTwo from "../../../../sections/Common/Header/HeaderTwo";
-import Breadcumb from "../../../../sections/About/Breadcumb";
+// import HeaderTwo from "../../../../sections/Common/Header/HeaderTwo";
+import Breadcumb from "~/sections/About/Breadcumb";
+import HeaderTwo from "~/sections/Common/Header/HeaderTwo";
 export default function ProjectPage() {
   return (
     <div>
